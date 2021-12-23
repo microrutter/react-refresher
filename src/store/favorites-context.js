2cab7afea79d5324aee3f9a177eaee4d57b3a06f
microrutter <microrutter2514@hotmail.com>
2021-12-23 10:45:00 +0000

    Complete Section

---
+++ src/store/favorites-context.js
@@ -0,0 +1,44 @@
+import { createContext, useState } from 'react'
+
+const FavoritesContext = createContext({
+  favorites: [],
+  totalFavorites: 0,
+  addFavorite: (favoriteMeetup) => {},
+  removeFavorite: (meetupId) => {},
+  itemIsFavorite: (meetupId) => {},
+})
+
+export function FavoritesContextProvider(props) {
+  const [userFavorites, setUserFavorites] = useState([])
+
+  function addFavoriteHandler(favoriteMeetup) {
+    setUserFavorites((prevUserFavorites) => {
+      return prevUserFavorites.concat(favoriteMeetup)
+    })
+  }
+
+  function removeFavoriteHandler(meetupId) {
+    setUserFavorites((prevUserFav) => {
+      return prevUserFav.filter((meetup) => meetup.id !== meetupId)
+    })
+  }
+
+  function itmeIsFavoriteHandler(meetupId) {
+    return userFavorites.some((meetup) => meetup.id === meetupId)
+  }
+
+  const context = {
+    favorites: userFavorites,
+    totalFavorites: userFavorites.length,
+    addFavorite: addFavoriteHandler,
+    removeFavorite: removeFavoriteHandler,
+    itemIsFavorite: itmeIsFavoriteHandler,
+  }
+  return (
+    <FavoritesContext.Provider value={context}>
+      {props.children}
+    </FavoritesContext.Provider>
+  )
+}
+
+export default FavoritesContext;
